b2d043bb184ae9570ac5d4e547924f194f815323
Houssam Righi <hrighi01@gmail.com>
2023-11-14 23:12:39 -0500

    Update styles for mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from '@/components/navbar'
-import LinkSea from '@/components/link-sea'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import LinkSea from "@/components/link-sea";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'IEEE Robowars 2024',
-  description: 'The official website for IEEE Robowars 2024',
-}
+  title: "IEEE Robowars 2024",
+  description: "The official website for IEEE Robowars 2024",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -28,5 +28,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#1DA1F2",
+      secondary: "#FFD55B",
+      tertiary: "#F45B69", // To be reconsidered
+      "light-grey": "#EDEDED",
+      "dark-grey": "#B7B7B7",
+      "landing-grey": "#383838",
+      black: "#000000",
+      white: "#FFFFFF",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      padding: {
+        "mobile-x": "32px",
+        "mobile-y": "64px",
+      },
+      gap: {
+        mobile: "32px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
