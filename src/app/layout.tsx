64db8206452895efa8a4a4892bb6a200f6079656
Houssam Righi <hrighi01@gmail.com>
2023-11-15 09:08:02 -0500

    Comment out Navbar and add About and Categories sections to Home page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <LinkSea />
         <Header />
-        <Navbar />
+        {/* <Navbar /> */}
         {children}
         <Footer />
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className=" bg-white">
-      <div className="mobile-section">
+      {/* About section */}
+      <div className="mobile-section bg-light-grey">
         <h1 className="mobile-h2 ">About</h1>
         <p className="mobile-p max-w-sm font-inter">
           RoboWars is the ultimate platform where the thunder of metal, the buzz
@@ -9,6 +10,18 @@ export default function Home() {
           battle; its a spectacle where amateur enthusiasts and seasoned
           engineers unveil their autonomous combatants to engage in fierce
           mechanical warfare.
+        </p>
+      </div>
+      <div className="h-2 w-full bg-white"></div>
+
+      {/* Categories */}
+      <div className="mobile-section">
+        <h1 className="mobile-h2 ">Categories</h1>
+        <p className="mobile-p max-w-sm font-inter">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </p>
       </div>
     </main>
