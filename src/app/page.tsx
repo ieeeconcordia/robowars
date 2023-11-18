ddb299fb0ceca132b0f838b4373a3b43105ca941
Webmaster-ieeeConcordia <114526522+Webmaster-ieeeConcordia@users.noreply.github.com>
2023-11-18 12:22:54 -0500

    Update About section in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,13 +35,13 @@ export default function Home() {
 
       {/* About section */}
       <div className="mobile-section text-black bg-light-grey">
-        <DropdownCard title="What is this?">
+        {/* <DropdownCard title="What is this?">
           The ultimate platform where the thunder of metal, the buzz of
           technology, and the thrill of competition converge. It's not just a
           battle; its a spectacle where amateur enthusiasts and seasoned
           engineers unveil their autonomous combatants to engage in fierce
           mechanical warfare.
-        </DropdownCard>
+        </DropdownCard> */}
 
         <h1 className="mobile-h2 ">About</h1>
         <p className="mobile-p max-w-sm font-inter">
