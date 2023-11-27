65dcc084e4ed1286405b55e9cef75a0c6389e7e5
Augusto Mota Pinheiro <augustomp55@gmail.com>
2023-11-26 20:18:18 -0500

    Added past robowars images

--- src/app/past-competitions/page.tsx
+++ src/app/past-competitions/page.tsx
@@ -1,17 +1,43 @@
 "use client";
 
 import SocialRow from "@/components/social-row/social-row";
-import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const images = [
-  "https://picsum.photos/200/300",
-  "https://picsum.photos/200/300",
-  "https://picsum.photos/200/300",
+const images2018 = [
+  "/images/past-robowars/2018/1.webp",
+  "/images/past-robowars/2018/2.webp",
+  "/images/past-robowars/2018/3.webp",
+  "/images/past-robowars/2018/4.webp",
+  "/images/past-robowars/2018/5.webp",
+  "/images/past-robowars/2018/6.webp",
+  "/images/past-robowars/2018/7.webp",
+  "/images/past-robowars/2018/8.webp",
+];
+
+const images2019 = [
+  "/images/past-robowars/2019/1.webp",
+  "/images/past-robowars/2019/2.webp",
+  "/images/past-robowars/2019/3.webp",
+  "/images/past-robowars/2019/4.webp",
+  "/images/past-robowars/2019/5.webp",
+  "/images/past-robowars/2019/6.webp",
+  "/images/past-robowars/2019/7.webp",
+];
+
+const images2023 = [
+  "/images/past-robowars/2023/1.webp",
+  "/images/past-robowars/2023/2.webp",
+  "/images/past-robowars/2023/3.webp",
+  "/images/past-robowars/2023/4.webp",
+  "/images/past-robowars/2023/5.webp",
+  "/images/past-robowars/2023/6.webp",
+  "/images/past-robowars/2023/7.webp",
+  "/images/past-robowars/2023/8.webp",
 ];
 
 export default function PastCompetitions() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="relative section h-[calc(100vh-6rem)] text-white overflow-hidden bg-black">
@@ -35,10 +61,10 @@ export default function PastCompetitions() {
 
       <div className="section text-black bg-light-grey">
         <h1 className="h2 ">Robowars 2023</h1>
-        <Carousel className="">
-          {images.map((image, index) => (
+        <Carousel className="max-w-screen-lg w-[90%]">
+          {images2023.map((image, index) => (
             <div key={index}>
-              <img src={image} alt="Robowars 2023" className="aspect-video" />
+              <img key={index} src={image} alt="Robowars 2023" className="aspect-video" />
             </div>
           ))}
         </Carousel>
@@ -46,8 +72,8 @@ export default function PastCompetitions() {
 
       <div className="section text-black bg-white">
         <h1 className="h2 ">Robowars 2019</h1>
-        <Carousel className="">
-          {images.map((image, index) => (
+        <Carousel className="max-w-screen-lg w-[90%]">
+          {images2019.map((image, index) => (
             <div key={index}>
               <img src={image} alt="Robowars 2023" className="aspect-video" />
             </div>
@@ -57,8 +83,8 @@ export default function PastCompetitions() {
 
       <div className="section text-black bg-light-grey">
         <h1 className="h2 ">Robowars 2018</h1>
-        <Carousel className="">
-          {images.map((image, index) => (
+        <Carousel className="max-w-screen-lg w-[90%]">
+          {images2018.map((image, index) => (
             <div key={index}>
               <img src={image} alt="Robowars 2023" className="aspect-video" />
             </div>
