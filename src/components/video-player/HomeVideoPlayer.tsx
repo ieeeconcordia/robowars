5e3d7cbb4b47dc1140490b27fbf593df328c9536
Houssam Righi <hrighi01@gmail.com>
2023-12-09 13:09:02 -0500

    Update Vimeo player settings

--- src/components/video-player/HomeVideoPlayer.tsx
+++ src/components/video-player/HomeVideoPlayer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HomeVideoPlayer() {
   return (
     <iframe
-      src="https://player.vimeo.com/video/892920365?title=0&byline=0&playinline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
+      src="https://player.vimeo.com/video/892920365?title=0&byline=0&pip=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
       allow="autoplay; fullscreen; picture-in-picture"
       className="aspect-video w-full max-w-screen-lg"
       title="Robowars 2024"
