4a46d2096f66800743f53e63af4f8fa0f954458b
Houssam Righi <hrighi01@gmail.com>
2023-12-09 13:41:40 -0500

    Update video player aspect ratio

--- src/components/video-player/HomeVideoPlayer.tsx
+++ src/components/video-player/HomeVideoPlayer.tsx
@@ -5,7 +5,7 @@ export default function HomeVideoPlayer() {
     <iframe
       src="https://player.vimeo.com/video/892920365?title=0&byline=0&pip=0&playsinline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
       allow="fullscreen"
-      className="aspect-video max-w-screen-lg min-w-[18rem] w-[70%]"
+      className="aspect-video w-full max-w-screen-lg min-w-[18rem]"
       title="Robowars 2024"
     ></iframe>
   );
